4420c704c7a9ff5de3bc41fbda46b30a027713f9
wongchunyick <yick@madmadgroup.com>
2016-09-23 20:37:34 +0800

    test03

--- cloud/main.js
+++ cloud/main.js
@@ -400,9 +400,10 @@ Parse.Cloud.define("SecretDisliked", function(request, response) {
 Hates A Post 7/12
 **************************************/
 Parse.Cloud.define("PostHates", function(request, response) {
+
   query = new Parse.Query("Secret");
   var uid=request.params.userId; 
-  query.get(request.params.secretId, {
+  query.get(request.params.secretId,{useMasterKey: true},{
     success: function(result) {
       result.increment("hates");
       result.save();
