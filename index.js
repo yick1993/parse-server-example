96b929a2a6600bc1ae71167bcf3d19f70622bfc5
wongchunyick <yick@madmadgroup.com>
2016-10-01 23:54:38 +0800

    00

--- index.js
+++ index.js
@@ -30,18 +30,18 @@ var api = new ParseServer({
   // liveQuery: {
   //   classNames: ["Secret", "Comment"] // List of classes to support for query subscriptions
   // },
-  //   push: {
-  //   android: {
-  //       senderId: '155059185604',
-  //       apiKey: 'AIzaSyDMEtG-JEyBPnGsvEihEXih0cVYlmMLMsc'
-  //   }
-  // }
+    push: {
+    android: {
+        senderId: '155059185604',
+        apiKey: 'AIzaSyDMEtG-JEyBPnGsvEihEXih0cVYlmMLMsc'
+    }
+  },
     push: pushConfig
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
-
+ 
 var app = express();
 
 // Serve static assets from the /public folder
